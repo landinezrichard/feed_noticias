fcd7a65d3b357b1a7e74ef8e35af42c1cef4c56f
Ricardo <landinezrichard@gmail.com>
2016-01-19 10:43:06 -0500

    Lista animación para ver la noticia

--- dev/components/News/index.js
+++ dev/components/News/index.js
@@ -1,20 +1,42 @@
 var showNews = (function () {
-    /* Dependencias*/
-    const $ = require('jquery');
-    const newsList = $('.News');
-    const clase    = 'News-itemFull--show';   
+	/* Dependencias*/
+	const $ = require('jquery');
+	const newsList = $('.News');
+	const clase    = 'News-itemFull--show';
+	let newsItem   = $('.News-itemFull'); 
 
-    function suscribeEvents () {
-        newsList.on('click','.News-item', function () {
-            $(this).find('.News-itemFull').toggleClass(clase);           
-        });
-    }    
+	// function suscribeEvents () {
+	// 	newsList.on('click','.News-item', function () {
+	// 		$(this).find('.News-itemFull').toggleClass(clase);
+	// 	});
+	// }
 
-    return {
-        init : function () {
-            suscribeEvents();
-        }
-    };
+	function suscribeEvents () {
+		newsItem.css('display','none');
+		// newsList.on('click','.News-item', function () {
+		
+		// 	$(this).find('.News-itemFull').slideToggle('slow');
+		// 	
+		// });
+
+		$('.News-itemLight').on('click', function () {
+			if($(this).next().is(':visible')){
+				$(this).next().slideUp();
+			}
+			if($(this).next().is(':hidden')){
+				$('.News-itemLight').next().slideUp();
+				$(this).next().slideDown();
+			}
+		});
+
+
+	}
+
+	return {
+		init : function () {
+			suscribeEvents();
+		}
+	};
 
 })();
 
